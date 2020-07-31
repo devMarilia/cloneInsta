cafb588cb5a31e44d8148689c0552cea03cb2073
devMarilia <marilia.lemos753@gmail.com>
2020-07-30 21:43:52 -0300

    Configuração de ambiente

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native'
+import Header from './pages/Header'
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header/>
+      </View>
+    );
+  }
+}
+
+export default App 
---
+++ src/pages/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Header extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Componente Header</Text>
+            </View>
+        );
+    }
+}
+
+export default Header 
